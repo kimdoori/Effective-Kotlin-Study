5cc0fb141568d3e3cda3090fdbf1240b2fe2f289
KimHyoJin <hyojin03131995@gmail.com>
2022-02-10 00:06:48 +0900

    chore : add sample file for template

---
+++ src/test/kotlin/com/toss/study/effectivekotlinstudy/item01/Item01Test.kt
@@ -0,0 +1,12 @@
+package com.toss.study.effectivekotlinstudy.item01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+class Item01Test {
+
+	@Test
+	fun sampleTest() {
+	}
+
+}
