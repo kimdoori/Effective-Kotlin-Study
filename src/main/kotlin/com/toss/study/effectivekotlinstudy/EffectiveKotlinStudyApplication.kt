c9ff840cb0b70a9486bbe988112495ef872e2671
KimHyoJin <hyojin03131995@gmail.com>
2022-02-09 21:32:39 +0900

    init : setup the project

---
+++ src/main/kotlin/com/toss/study/effectivekotlinstudy/EffectiveKotlinStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.toss.study.effectivekotlinstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EffectiveKotlinStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<EffectiveKotlinStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/toss/study/effectivekotlinstudy/EffectiveKotlinStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toss.study.effectivekotlinstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EffectiveKotlinStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
